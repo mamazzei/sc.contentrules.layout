3a8947943b1db71f3ce264041e19f32a0e56d392
Érico Andrei <erico@simplesconsultoria.com.br>
2012-10-29 16:31:41 -0200

    Add tests

--- src/sc/contentrules/layout/tests/test_action.py
+++ src/sc/contentrules/layout/tests/test_action.py
@@ -105,7 +105,7 @@ class TestSetLayoutAction(unittest.TestCase):
         self.assertEquals(hasattr(self.sub_folder, 'layout'), False)
 
 
-class TestActionWithCondition(unittest.TestCase):
+class TestViewsVocab(unittest.TestCase):
 
     layer = INTEGRATION_TESTING
 
@@ -116,6 +116,122 @@ class TestActionWithCondition(unittest.TestCase):
         self.folder = self.portal['folder']
         sub_folder_id = self.folder.invokeFactory('Folder', 'sub_folder')
         self.sub_folder = self.folder[sub_folder_id]
+        storage = getUtility(IRuleStorage)
+        storage[u'foo'] = Rule()
+        self.rule = self.portal.restrictedTraverse('++rule++foo')
+
+    def _add_condition(self, rule, types=['Folder', ]):
+        element = getUtility(IRuleCondition,
+                             name='plone.conditions.PortalType')
+        adding = getMultiAdapter((rule, self.portal.REQUEST),
+                                 name='+condition')
+        addview = getMultiAdapter((adding, self.portal.REQUEST),
+                                  name=element.addview)
+
+        addview.createAndAdd(data={'check_types': types})
+
+    def _add_action(self, rule, view='_default_view'):
+        element = getUtility(IRuleAction,
+                             name='sc.contentrules.actions.layout')
+        adding = getMultiAdapter((rule, self.portal.REQUEST),
+                                 name='+action')
+        addview = getMultiAdapter((adding, self.portal.REQUEST),
+                                  name=element.addview)
+
+        addview.createAndAdd(data={'layout': view})
+
+    def testRuleWithoutCondition(self):
+        rule = self.rule
+        self._add_action(rule)
+        util = getUtility(IVocabularyFactory, VOCAB)
+        vocab = util(rule)
+        self.assertEquals(len(vocab.by_token), 1)
+
+    def testRuleWithCondition(self):
+        rule = self.rule
+        self._add_condition(rule)
+        self._add_action(rule)
+        util = getUtility(IVocabularyFactory, VOCAB)
+        vocab = util(rule)
+        self.assertEquals(len(vocab.by_token), 6)
+
+    def testRuleWithConditionAndTwoTypes(self):
+        rule = self.rule
+        # Folder and Document have no intersection between their
+        # available views, so our rule should return just _default_view
+        self._add_condition(rule, ['Folder', 'Document'])
+        self._add_action(rule)
+        util = getUtility(IVocabularyFactory, VOCAB)
+        vocab = util(rule)
+        self.assertEquals(len(vocab.by_token), 1)
+
+
+class TestActionAndCondition(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('Folder', 'folder')
+        self.folder = self.portal['folder']
+        sub_folder_id = self.folder.invokeFactory('Folder', 'sub_folder')
+        self.sub_folder = self.folder[sub_folder_id]
+        storage = getUtility(IRuleStorage)
+        storage[u'foo'] = Rule()
+        self.rule = self.portal.restrictedTraverse('++rule++foo')
+
+    def _add_condition(self, rule, types=['Folder', ]):
+        element = getUtility(IRuleCondition,
+                             name='plone.conditions.PortalType')
+        adding = getMultiAdapter((rule, self.portal.REQUEST),
+                                 name='+condition')
+        addview = getMultiAdapter((adding, self.portal.REQUEST),
+                                  name=element.addview)
+
+        addview.createAndAdd(data={'check_types': types})
+
+    def _add_action(self, rule, view='_default_view'):
+        element = getUtility(IRuleAction,
+                             name='sc.contentrules.actions.layout')
+        adding = getMultiAdapter((rule, self.portal.REQUEST),
+                                 name='+action')
+        addview = getMultiAdapter((adding, self.portal.REQUEST),
+                                  name=element.addview)
+
+        addview.createAndAdd(data={'layout': view})
+
+    def testActionWithoutPTCondition(self):
+        rule = self.rule
+        self._add_action(rule)
+        e = rule.actions[0]
+        ex = getMultiAdapter((self.folder, e,
+                             DummyEvent(self.sub_folder)),
+                             IExecutable)
+        self.assertEquals(True, ex())
+        # Layout should not be set (_default_view does nothing)
+        self.failIf(hasattr(self.sub_folder, 'layout'))
+
+    def testActionWithPTCondition(self):
+        rule = self.rule
+        self._add_condition(rule)
+        self._add_action(rule, 'folder_summary_view')
+        condition = rule.conditions[0]
+        action = rule.actions[0]
+
+        # Condition should be met
+        ex = getMultiAdapter((self.folder, condition,
+                             DummyEvent(self.sub_folder)),
+                             IExecutable)
+        self.assertEquals(True, ex())
+
+        # Action will be executed
+        ex = getMultiAdapter((self.folder, action,
+                             DummyEvent(self.sub_folder)),
+                             IExecutable)
+        self.assertEquals(True, ex())
+
+        self.assertEquals(self.sub_folder.layout, 'folder_summary_view')
 
 
 def test_suite():
