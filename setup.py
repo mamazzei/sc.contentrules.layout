7a6fd9f4b4475c8235806c8953205f9a3e5d2783
hvelarde <hector.velarde@gmail.com>
2013-04-01 16:00:22 -0300

    fix package distribution

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 # -*- coding:utf-8 -*-
 
+from setuptools import find_packages
+from setuptools import setup
+
 import os
-from setuptools import setup, find_packages
 
 version = '1.0b3.dev0'
-long_description = (open("README.txt").read() + "\n" +
-                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
-                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" +
-                    open(os.path.join("docs", "HISTORY.txt")).read())
-
+long_description = (open("README.rst").read() + "\n" +
+    open(os.path.join("docs", "INSTALL.rst")).read() + "\n" +
+    open(os.path.join("docs", "CREDITS.rst")).read() + "\n" +
+    open(os.path.join("docs", "HISTORY.rst")).read())
 
 setup(name='sc.contentrules.layout',
       version=version,
@@ -21,17 +22,20 @@ setup(name='sc.contentrules.layout',
           "Framework :: Plone :: 4.2",
           "Framework :: Plone :: 4.3",
           "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
           "Operating System :: OS Independent",
           "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
           "Topic :: Internet :: WWW/HTTP",
           "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-          "Topic :: Software Development :: Libraries :: Python Modules"],
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='plone contentrules action',
       author='Simples Consultoria',
       author_email='products@simplesconsultoria.com.br',
       url='http://www.simplesconsultoria.com.br/',
-      license='GPL',
+      license='GPLv2',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       namespace_packages=['sc', 'sc.contentrules'],
